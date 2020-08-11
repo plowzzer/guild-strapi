1b9ea9a2b69bafc3c14c925bd3a3416e49cd76d1
Pedro Pizzo de Paula <poloo.p3@gmail.com>
2020-08-11 17:43:07 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '87ef7a0c6b07e8aa4554267341aa288a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ea83d6c3-79c5-40d6-b071-05271242d6f3'
+};
